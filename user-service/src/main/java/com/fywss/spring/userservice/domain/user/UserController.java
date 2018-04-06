8cc9cceaf8c78d391dff0199cd70f3dc4c51a765
Steve Kotsopoulos <sk@fywss.com>
2018-04-05 20:56:14 -0400

    add cross origin header

--- user-service/src/main/java/com/fywss/spring/userservice/domain/user/UserController.java
+++ user-service/src/main/java/com/fywss/spring/userservice/domain/user/UserController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import io.swagger.annotations.ApiOperation;
 
 @ControllerAdvice
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:5000"})
 @RequestMapping ("/api/v1/users")
 public class UserController {
 	
